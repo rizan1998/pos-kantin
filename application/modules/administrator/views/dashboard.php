8d6b02df79150f577a45ee9b3cff6420ff42fc2c
rizan1998 <rizanalfalah@gmail.com>
2023-03-09 11:15:35 +0700

    remove button download in dashboard

--- application/modules/administrator/views/dashboard.php
+++ application/modules/administrator/views/dashboard.php
@@ -11,18 +11,18 @@ $this->load->view('parts/left_menu_part');
 <div class="block-header">
     <div class="row">
         <div class="col-lg-6 col-md-6 col-sm-12">
-            <h2>eCommerce</h2>
+            <h2>Kasir dashboard</h2>
             <ul class="breadcrumb">
-                <li class="breadcrumb-item"><a href="index.html"><i class="fa fa-dashboard"></i></a></li>
+                <!-- <li class="breadcrumb-item"><a href="index.html"><i class="fa fa-dashboard"></i></a></li>
                 <li class="breadcrumb-item">Dashboard</li>
-                <li class="breadcrumb-item active">eCommerce</li>
+                <li class="breadcrumb-item active">eCommerce</li> -->
             </ul>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12">
             <div class="d-flex flex-row-reverse">
                 <div class="page_action">
-                    <button type="button" class="btn btn-primary"><i class="fa fa-download"></i> Download report</button>
-                    <button type="button" class="btn btn-secondary"><i class="fa fa-send"></i> Send report</button>
+                    <!-- <button type="button" class="btn btn-primary"><i class="fa fa-download"></i> Download report</button> -->
+                    <!-- <button type="button" class="btn btn-secondary"><i class="fa fa-send"></i> Send report</button> -->
                 </div>
                 <div class="p-2 d-flex">
 
